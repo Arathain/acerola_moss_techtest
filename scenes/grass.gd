983fd72719ab39cf74309bb7aabb6f54898f4ea9
Arathain <76701473+Arathain@users.noreply.github.com>
2023-11-01 19:44:18 +0100

    Upload the actual files

---
+++ scenes/grass.gd
@@ -0,0 +1,27 @@
+@tool
+
+extends StaticBody3D
+
+@export var shellAmount : int = 32
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var shells = Node3D.new();
+	shells.name = "Shells"
+	for n in shellAmount:
+		var scene = preload("res://scenes/base_mesh.tscn");
+		var node = scene.instantiate();
+		var mat = node.get_surface_override_material(0).duplicate();
+		mat.set_shader_parameter("shellIndex", n);
+		mat.set_shader_parameter("shellCount", shellAmount);
+		node.set_surface_override_material(0, mat);
+		shells.add_child(node);
+		# node.set_owner(get_tree().edited_scene_root);
+	add_child(shells)
+	shells.set_owner(get_tree().edited_scene_root);
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
